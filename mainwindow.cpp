36b0dac1738eb3fecc079996af1376148b260b06
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-09-23 11:05:38 +0300

    Add table

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "mytablemodel.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableView->setModel(new MyTableModel());
 }
 
 MainWindow::~MainWindow()
