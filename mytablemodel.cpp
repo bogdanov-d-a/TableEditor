109f595ccaa1961cbb0e0d83cb9d8a3185eaf3ad
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-09-23 07:58:39 +0300

    Overload MyTableModel methods

--- mytablemodel.cpp
+++ mytablemodel.cpp
@@ -2,6 +2,24 @@
 
 MyTableModel::MyTableModel()
 {
-
 }
 
+int MyTableModel::rowCount(const QModelIndex &parent) const
+{
+}
+
+int MyTableModel::columnCount(const QModelIndex &parent) const
+{
+}
+
+QVariant MyTableModel::data(const QModelIndex &index, int role) const
+{
+}
+
+bool MyTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+}
+
+Qt::ItemFlags MyTableModel::flags(const QModelIndex &index) const
+{
+}
--- mytablemodel.h
+++ mytablemodel.h
@@ -1,11 +1,17 @@
 #ifndef MYTABLEMODEL_H
 #define MYTABLEMODEL_H
 
+#include <QAbstractTableModel>
 
 class MyTableModel : public QAbstractTableModel
 {
 public:
     MyTableModel();
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    bool setData(const QModelIndex &index, const QVariant &value, int role);
+    Qt::ItemFlags flags(const QModelIndex &index) const;
 };
 
 #endif // MYTABLEMODEL_H
