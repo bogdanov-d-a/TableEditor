963d285ae0f6d88c58fe4251d89d639e964655c0
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-09-23 10:38:39 +0300

    Implement model

--- mytablemodel.cpp
+++ mytablemodel.cpp
@@ -1,25 +1,31 @@
 #include "mytablemodel.h"
 
-MyTableModel::MyTableModel()
-{
-}
-
 int MyTableModel::rowCount(const QModelIndex &parent) const
 {
+    return m_data.size();
 }
 
 int MyTableModel::columnCount(const QModelIndex &parent) const
 {
+    return 2;
 }
 
 QVariant MyTableModel::data(const QModelIndex &index, int role) const
 {
+    Record const& dataElem = m_data[index.row()];
+
+    if (index.column() == 0)
+        return QVariant(dataElem.label);
+    else
+        return QVariant(dataElem.number);
 }
 
 bool MyTableModel::setData(const QModelIndex &index, const QVariant &value, int role)
 {
+    return false;
 }
 
 Qt::ItemFlags MyTableModel::flags(const QModelIndex &index) const
 {
+    return Qt::ItemFlags();
 }
--- mytablemodel.h
+++ mytablemodel.h
@@ -6,12 +6,19 @@
 class MyTableModel : public QAbstractTableModel
 {
 public:
-    MyTableModel();
     int rowCount(const QModelIndex &parent) const;
     int columnCount(const QModelIndex &parent) const;
     QVariant data(const QModelIndex &index, int role) const;
     bool setData(const QModelIndex &index, const QVariant &value, int role);
     Qt::ItemFlags flags(const QModelIndex &index) const;
+
+private:
+    typedef struct {
+        QString label;
+        unsigned int number;
+    } Record;
+
+    QVector<Record> m_data;
 };
 
 #endif // MYTABLEMODEL_H
