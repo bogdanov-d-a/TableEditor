49741454a06a5373c1ca27b21b5d4ed5d21f9464
bogdanov-d-a <bogdanov.d.a.1994@gmail.com>
2015-10-02 00:10:01 +0300

    Add plot colors

--- TableEditor.pro
+++ TableEditor.pro
@@ -10,6 +10,7 @@ greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 TARGET = TableEditor
 TEMPLATE = app
+CONFIG += C++11
 
 
 SOURCES += main.cpp\
--- plotwidget.cpp
+++ plotwidget.cpp
@@ -7,13 +7,22 @@ PlotWidget::PlotWidget(MyTableModel *table, QWidget *parent)
     ,m_table(table)
 {}
 
+const QVector<QColor> colors = {
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "pink"
+};
+
 void PlotWidget::paintEvent(QPaintEvent *event)
 {
     QWidget::paintEvent(event);
 
     QPainter painter(this);
     painter.setRenderHints(painter.renderHints() | QPainter::Antialiasing);
-    painter.drawRect(0, 0, size().width() - 1, size().height() - 1);
+    //painter.drawRect(0, 0, size().width() - 1, size().height() - 1);
 
     int sum = 0;
     for (int row = 0; row < m_table->getSize(); ++row)
@@ -26,6 +35,11 @@ void PlotWidget::paintEvent(QPaintEvent *event)
     for (int row = 0; row < m_table->getSize(); ++row)
     {
         int curSize = 360 * 16 * m_table->getNumber(row) / sum;
+
+        if (row == 0)
+            painter.setBrush(QBrush("red"));
+        else
+            painter.setBrush(QBrush(colors[(row - 1) % colors.size()]));
 
         painter.drawPie(0, 0,
                         size().width(), size().height(),
